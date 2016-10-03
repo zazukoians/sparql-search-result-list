181c4ce5e736bd6f1e2afecc9c241a3d63d9111b
bergi <bergi@axolotlfarm.org>
2016-10-03 21:34:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# sparql-search-result-list
+
+HTML search result list for SPARQL queries with paging support.
+
+## Usage
+
+The constructor has only an options argument with the  
+
+    var SparqlSearchResultList = require('sparql-search-result-list')
+
+    var resultList = new SparqlSearchResultList({...})
+
+### SPARQL & dataset options 
+
+- `endpointUrl`: URL of the SPARQL endpoint (required)
+- `pageSize`: Number of rows a page contains (required)
+- `preload`: Number of rows which will be loaded ahead before and after the current view (default: 50)
+- `resultType`: The rdf:type of the results (required)
+- `metadataQueryTemplate`: SPARQL template for search metadata  (required)
+- `resultQueryTemplate`: SPARQL template for search results (required)
+
+#### SPARQL query templates
+
+The templates must be given as a string.
+`${searchString}` will be replaced by the search string.
+`${offset}` and `${limit}` will be replaced in the result query with the offset and limit of the current page.
+
+Dynamic templates can be implemented by replacing the `buildMetadataFilterQuery` and `buildResultFilterQuery` methods.
+
+### Render options
+
+- `scrollArea`: ID of the div block for the scroll area (default: `scrollArea`)
+- `contentArea`: ID of the div block for the content area (default: `contentArea`)
+- `dummyResult`: HTML as string that will be used until the row is loaded.
+  This will be also used to calculate the row height! (required)
+- `renderResult(Graph graph, NamedNode result)`: The function which renders the result row.
+  The graph contains all results of the page.
+  result is the subject of the current result row. (required)
+
+#### HTML
+
+[clusterize.js-paging](https://github.com/zazukoians/clusterize.js-paging) is used to render the result list.
+The following HTML structure is required to render the list:
+
+    <div id="scrollArea" class="clusterize-scroll">
+      <div id="contentArea" class="clusterize-content">
+        <div class="clusterize-no-data">Loading data…</div>
+      </div>
+    </div>
+
+### Events
+
+- `onResultRendered()`: Will be called after the current pages have been rendered
+- `onFetched()`: Will be called after each result SPARQL query
+- `onFetching()`: Will be called before each result SPARQL query
+- `onResultMetadata(metadata)`: Will be called after the metadata has been fetched.
+ It contains the `length`, `start` and `end`.
+
+### Methods
+
+- `search(searchString)`: Starts a new search with the given search string. 
---
+++ index.js
@@ -0,0 +1,141 @@
+var rdfFetch = require('rdf-fetch')
+var SparqlClient = require('sparql-http-client')
+var ClusterizePaging = require('clusterize.js-paging')
+
+function SparqlSearchResultList (options) {
+  this.options = options || {}
+
+  this.client = new SparqlClient({
+    fetch: rdfFetch,
+    endpointUrl: options.endpointUrl,
+    updateUrl: options.endpointUrl
+  })
+
+  this.rows = []
+
+  this.start = ''
+  this.end = ''
+
+  this.clusterize = new ClusterizePaging({
+    rows: this.rows,
+    scrollId: options.scrollArea || 'scrollArea',
+    contentId: options.contentArea || 'contentArea',
+    dummyRow: options.dummyResult,
+    no_data_text: 'No results found matching the filters.',
+    pageSize: options.pageSize,
+    preload: options.preload || 50,
+    callbacks: {
+      loadRows: this.loadRows.bind(this),
+      rowsLoaded: options.onResultRendered
+    }
+  })
+}
+
+SparqlSearchResultList.prototype.search = function (query) {
+  var self = this
+
+  this.query = query
+
+  if (this.options.onFetching) {
+    this.options.onFetching()
+  }
+
+  return this.fetchResultLength().then(function (length) {
+    var metadata = {
+      length: length,
+      start: self.start,
+      end: self.end
+    }
+
+    if (self.options.onResultMetadata) {
+      self.options.onResultMetadata(metadata)
+    }
+
+    if (self.options.onFetched) {
+      self.options.onFetched()
+    }
+
+    return self.clusterize.init(length)
+  })
+}
+
+SparqlSearchResultList.prototype.buildMetadataFilterQuery = function () {
+  return this.options.metadataQueryTemplate
+}
+
+SparqlSearchResultList.prototype.buildMetadataQuery = function () {
+  return this.buildMetadataFilterQuery().replace(/\${searchString}/g, this.query)
+}
+
+SparqlSearchResultList.prototype.fetchResultLength = function () {
+  var self = this
+  var query = this.buildMetadataQuery()
+
+  return this.client.postQuery(query).then(function (res) {
+    var count = res.graph.match(null, 'http://voc.zazuko.com/zack#numberOfResults').toArray().shift()
+
+    var querystart = res.graph.match(null, 'http://voc.zazuko.com/zack#queryStart').toArray().shift()
+    var queryend = res.graph.match(null, 'http://voc.zazuko.com/zack#queryEnd').toArray().shift()
+
+    if (!querystart && !queryend) {
+      self.start = ''
+      self.end = ''
+    } else {
+      self.start = new Date(querystart.object.nominalValue)
+      self.end = new Date(queryend.object.nominalValue)
+    }
+
+    if (!count) {
+      return 0
+    }
+
+    return parseInt(count.object.nominalValue)
+  })
+}
+
+SparqlSearchResultList.prototype.buildResultFilterQuery = function () {
+  return this.options.resultQueryTemplate
+}
+
+SparqlSearchResultList.prototype.buildResultQuery = function (offset) {
+  return this.buildResultFilterQuery()
+    .replace('${searchString}', this.query) // eslint-disable-line no-template-curly-in-string
+    .replace('${offset}', offset) // eslint-disable-line no-template-curly-in-string
+    .replace('${limit}', this.options.pageSize) // eslint-disable-line no-template-curly-in-string
+}
+
+SparqlSearchResultList.prototype.fetchPage = function (offset) {
+  var query = this.buildResultQuery(offset)
+
+  return this.client.postQuery(query).then(function (res) {
+    return res.graph
+  })
+}
+
+SparqlSearchResultList.prototype.resultSubjects = function (page) {
+  return page.match(null, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', this.options.resultType).map(function (triple) {
+    return triple.subject
+  })
+}
+
+SparqlSearchResultList.prototype.loadRows = function (offset) {
+  var self = this
+
+  if (this.options.onFetching) {
+    this.options.onFetching()
+  }
+
+  return this.fetchPage(offset).then(function (page) {
+    var subjects = self.resultSubjects(page)
+
+    if (self.options.onFetched) {
+      self.options.onFetched()
+    }
+
+    return subjects.map(function (subject, index) {
+      return self.options.renderResult(page, subject)
+    })
+  })
+}
+
+module.exports = SparqlSearchResultList
